7258baf3efb149dfc248143b5e685995dcccb570
Ícaro Forte <icaro_forte@hotmail.com>
2018-06-25 19:23:13 -0300

    Alguns ajustes

--- blackJack_Haskell.hs
+++ blackJack_Haskell.hs
@@ -1,13 +1,16 @@
 import System.Random
 import Control.Monad (replicateM)
 import System.IO.Unsafe
+import Control.Monad(when) 
  
 baralho = zip ["As de copas", "As de espadas", "As de ouro", "As de paus", "Dois de copas","Dois de espadas", "Dois de ouro", "Dois de paus", "Tres de copas", "Tres de espadas", "Tres de ouro", "Tres de paus", "Quatro de copas", "Quatro de espadas", "Quatro de ouro", "Quatro de paus", "Cinco de copas", "Cinco de espadas", "Cinco de ouro", "Cinco de paus", "Seis de copas", "Seis de espadas", "Seis de ouro", "Seis de paus", "Sete de copas", "Sete de espadas", "Sete de ouro", "Sete de paus", "Oito de copas", "Oito de espadas", "Oito de ouro", "Oito de paus", "Nove de copas", "Nove de espadas", "Nove de ouro", "Nove de paus", "Dez de copas", "Dez de espadas", "Dez de ouro", "Dez de paus", "Valete de copas", "Valete de espadas", "Valete de ouro", "Valete de paus", "Dama de copas", "Dama de espadas", "Dama de ouro", "Dama de paus", "Rei de copas", "Rei de espadas", "Rei de ouro", "Rei de paus"] [1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10]
 
 tamanhoBaralho = length baralho
 
---retorna um valor do tipo IO Int que é diferente de Int e não posso usar ele no método adicionarCartaBaralho
-valor = randomRIO (0, tamanhoBaralho)
+valor = 0
+
+--valor = take 10 $ randomRs (0,51) (mkStdGen 192391) :: [Int]
+--retorna uma lista de 10 valores aleatórios, o problema tá no mkStdGen que vai fazer a lista ser sempre igual
 
 baralhoHumano = zip [][]
 
@@ -20,16 +23,23 @@ para adicionar uma carta use:
 baralhoHumano = adicionarCartaBaralho 7
 
 -}
-adicionarCartaBaralho :: (Integral humano) => humano -> ([Char],Int)   
-adicionarCartaBaralho 7 = baralho!!1 --devia usar valor aqui, mas o tipo é diferente  
-adicionarCartaBaralho x = ("casoDeErroParaTeste",0)  
+--adicionarCartaBaralho :: (Integral humano) => humano -> ([Char],Int)   
+--adicionarCartaBaralho 7 = baralho!!valor --devia usar valor aqui, mas o tipo é diferente  
+--adicionarCartaBaralho x = ("casoDeErroParaTeste",0)  
+
+adicionarCartaBaralho num = baralho !! num
 
 
-main = do  
-    putStrLn "Bem vindo ao jogo 21!!! Digite seu nome:"  
-    name <- getLine  
-    putStrLn ("Iaew " ++ name ++ ", cê é zika")  
+geraValoresAleatorio numero = take 20 $ randomRs (0,51) (mkStdGen numero) :: [Int]
+	
 
+	
+valorEntrada = do
+	putStrLn "Insira um valor inteiro para gerar random:"
+        num <- readLn :: IO Int
+        return $ num
+        
+        
 {- Comentário  em Haskell
 
 baralho !! 0 (pega a posição 0 da tuplha baralho) ["As de copas", 1]
