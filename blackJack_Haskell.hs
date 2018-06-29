8755e9b76d217ea49f1de40d69136bb439892814
Ícaro Forte <icaro_forte@hotmail.com>
2018-06-28 22:14:58 -0300

    Jogo acaba quando jogadorHumano alcança valor maior que 21 pontos

--- blackJack_Haskell.hs
+++ blackJack_Haskell.hs
@@ -1,7 +1,8 @@
 import System.Random as Random
 import Control.Monad (replicateM)
 import System.IO.Unsafe
-import Control.Monad(when) 
+import Control.Monad(when)
+import System.Exit (exitSuccess) 
 
 baralho = zip ["As de copas", "As de espadas", "As de ouro", "As de paus", "Dois de copas","Dois de espadas", "Dois de ouro", "Dois de paus", "Tres de copas", "Tres de espadas", "Tres de ouro", "Tres de paus", "Quatro de copas", "Quatro de espadas", "Quatro de ouro", "Quatro de paus", "Cinco de copas", "Cinco de espadas", "Cinco de ouro", "Cinco de paus", "Seis de copas", "Seis de espadas", "Seis de ouro", "Seis de paus", "Sete de copas", "Sete de espadas", "Sete de ouro", "Sete de paus", "Oito de copas", "Oito de espadas", "Oito de ouro", "Oito de paus", "Nove de copas", "Nove de espadas", "Nove de ouro", "Nove de paus", "Dez de copas", "Dez de espadas", "Dez de ouro", "Dez de paus", "Valete de copas", "Valete de espadas", "Valete de ouro", "Valete de paus", "Dama de copas", "Dama de espadas", "Dama de ouro", "Dama de paus", "Rei de copas", "Rei de espadas", "Rei de ouro", "Rei de paus"] [1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10]
 
@@ -64,7 +65,6 @@ iniciarPartida = do
    let novoBaralhoHumano = baralhoHumano ++ cartas
    putStrLn "Baralho Humano: "
    putStrLn((show novoBaralhoHumano) ++ " Pontuação: " ++ (show pontuacaoHumano2))
-  
 
    
 ------------------------------------------------------
@@ -107,24 +107,25 @@ iniciarPartida = do
    
 -- Exibe o vencedor baseado nos pontos feitos pelos jogadores   
 vencedorPartida pontuacaoHumano pontuacaoMaquina = do
-	putStrLn "\n----------- Resultado ----------------"
-	if ((pontuacaoHumano > pontuacaoMaquina) && (pontuacaoHumano <= 21)) then putStrLn ("Jogador humano é o vencedor: " ++ (show pontuacaoHumano) ++ " pontos" ++ "\nPontuação máquina: " ++ show(pontuacaoMaquina))
-	else if ((pontuacaoMaquina > pontuacaoHumano) && (pontuacaoMaquina <= 21)) then putStrLn ("Jogador maquina é o vencedor: " ++ (show pontuacaoMaquina) ++ " pontos" ++ "\nPontuação humano: " ++ show(pontuacaoHumano))
+	putStrLn "\n----------- Resultado Final ----------------"
+	if ((pontuacaoHumano > pontuacaoMaquina) && (pontuacaoHumano <= 21)) then putStrLn ("Jogador humano é o vencedor: " ++ (show pontuacaoHumano) ++ " pontos" ++ "\nPontuação da máquina: " ++ show(pontuacaoMaquina))
+	else if ((pontuacaoMaquina > pontuacaoHumano) && (pontuacaoMaquina <= 21)) then putStrLn ("Jogador maquina é o vencedor: " ++ (show pontuacaoMaquina) ++ " pontos" ++ "\nPontuação do humano: " ++ show(pontuacaoHumano))
 	else if (pontuacaoHumano == pontuacaoMaquina) then putStrLn ("Deu empate\n Jogador humano: " ++ (show pontuacaoHumano) ++ "\n Jogador maquina: " ++ show(pontuacaoMaquina))
-	else if (pontuacaoHumano > 21 && pontuacaoMaquina <= 21) then putStrLn ("Jogador maquina é o vencedor: " ++ (show pontuacaoMaquina) ++ " pontos" ++ "\nPontuação humano: " ++ show(pontuacaoHumano))
-	else if (pontuacaoMaquina > 21 && pontuacaoHumano <= 21) then putStrLn ("Jogador humano é o vencedor: " ++ (show pontuacaoHumano) ++ " pontos" ++ "\nPontuação máquina: " ++ show(pontuacaoMaquina))
+	else if (pontuacaoHumano > 21 && pontuacaoMaquina <= 21) then putStrLn ("Jogador maquina é o vencedor: " ++ (show pontuacaoMaquina) ++ " pontos" ++ "\nPontuação do humano: " ++ show(pontuacaoHumano))
+	else if (pontuacaoMaquina > 21 && pontuacaoHumano <= 21) then putStrLn ("Jogador humano é o vencedor: " ++ (show pontuacaoHumano) ++ " pontos" ++ "\nPontuação da máquina: " ++ show(pontuacaoMaquina))
+	
 	--Eu acho que aqui podemos reinvocar o inicarPartida e rodar um novo jogo quando der empate de pontos
 	else putStrLn ("Não houve ganhadores, ambos jogadores marcaram acima de 21 pontos! \nPontuação humano: " ++ (show pontuacaoHumano) ++ "\nPontuação máquina: " ++ (show pontuacaoMaquina))
-
+	
 -- Parâmetros: Escolha - TamanhoBaralho - PontuaçãoJogadorHumano - PontuaçãoJogadorMáquina - BaralhoJogadorHumano - BaralhoJogadorMáquina - Baralho
 puxarCarta :: Int -> Int -> Int -> Int -> [(String, Int)] -> [(String, Int)]-> [(String, Int)] -> IO()
 puxarCarta 2 tamanhoBaralho pontuacaoJogadorHumano pontuacaoJogadorMaquina baralhoJogadorHumano baralhoJogadorMaquina baralho = do
   --Isso aqui não tá fazendo nada a não ser imprimindo o resultado do baralho do jogador
   vencedorPartida pontuacaoJogadorHumano pontuacaoJogadorMaquina
-  --putStrLn(show(baralhoJogadorHumano) ++ " Pontuação:" ++ (show pontuacaoJogadorHumano))
-
+  
+  
 puxarCarta escolha tamanhoBaralho pontuacaoJogadorHumano pontuacaoJogadorMaquina baralhoJogadorHumano baralhoJogadorMaquina baralho = do
-
+  
   novoRandom <- randomRIO(0 :: Int, tamanhoBaralho :: Int)
   --Pegando nova carta
   let novaCarta = baralho !! novoRandom
@@ -139,15 +140,14 @@ puxarCarta escolha tamanhoBaralho pontuacaoJogadorHumano pontuacaoJogadorMaquina
   
   putStrLn("Baralho: \n" ++ (show novoBaralhoJogadorHumano) ++ " Pontuação: " ++ (show novaPontuacaoJogadorHumano))
   
-  print novoBaralho
-  
+  if (novaPontuacaoJogadorHumano > 21 || pontuacaoJogadorMaquina > 21) then vencedorPartida novaPontuacaoJogadorHumano pontuacaoJogadorMaquina
+  else do 
   putStrLn("Quer puxar outra carta? 1 - sim | 2 - não")
 
---tá faltando remover a carta!
   escolhaTemp <- getLine
   let escolha = read escolhaTemp :: Int
   
-   -- esse índice aqui tem de atualizar também 
+  --Atualizando índice
   let novoTamanhoBaralho = (tamanhoBaralho - 1)
 
   puxarCarta escolha novoTamanhoBaralho novaPontuacaoJogadorHumano pontuacaoJogadorMaquina novoBaralhoJogadorHumano baralhoJogadorMaquina novoBaralho
